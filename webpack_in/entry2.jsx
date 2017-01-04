526d43d4637467d90fe68eac15b4a453f5122f5f
Marat Nepomnyashy <maratbn@gmail.com>
2017-01-03 20:53:31 -0800

    Encased the common logic for the actions to include and exclude tokens into a common switch/case.

--- webpack_in/entry2.jsx
+++ webpack_in/entry2.jsx
@@ -67,33 +67,38 @@ const createActionToExcludeToken = (post_id, strToken) => {
 
 
 const reducer = (state = {}, action) => {
-          const { post_id, token } = action;
+          switch(action.type) {
+            case ACTION__EXCLUDE_TOKEN:
+            case ACTION__INCLUDE_TOKEN:
 
-          const arrTokensOld = state[post_id] && state[post_id].data || ARR_EMPTY;
+              const { post_id, token } = action;
 
-          if (action.type == ACTION__EXCLUDE_TOKEN) {
-            var indexToken = arrTokensOld.indexOf(token);
-            if (indexToken != -1) {
-              return {
-                  ...state,
-                  [post_id]: {
-                      ...state[post_id],
-                      data: arrTokensOld.slice(0, indexToken)
-                                        .concat(arrTokensOld.slice(indexToken + 1,
-                                                                   arrTokensOld.length))
-                    }
-                };
-            }
-          } else if (action.type == ACTION__INCLUDE_TOKEN) {
-            if (arrTokensOld.indexOf(token) == -1) {
-              return {
-                  ...state,
-                  [post_id]: {
-                      ...state[post_id],
-                      data: [...arrTokensOld, token].sort()
-                    }
-                };
-            }
+              const arrTokensOld = state[post_id] && state[post_id].data || ARR_EMPTY;
+
+              if (action.type == ACTION__EXCLUDE_TOKEN) {
+                var indexToken = arrTokensOld.indexOf(token);
+                if (indexToken != -1) {
+                  return {
+                      ...state,
+                      [post_id]: {
+                          ...state[post_id],
+                          data: arrTokensOld.slice(0, indexToken)
+                                            .concat(arrTokensOld.slice(indexToken + 1,
+                                                                       arrTokensOld.length))
+                        }
+                    };
+                }
+              } else if (action.type == ACTION__INCLUDE_TOKEN) {
+                if (arrTokensOld.indexOf(token) == -1) {
+                  return {
+                      ...state,
+                      [post_id]: {
+                          ...state[post_id],
+                          data: [...arrTokensOld, token].sort()
+                        }
+                    };
+                }
+              }
           }
 
           return state;
