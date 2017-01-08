d4d8805eeebd71c4fd7c616e955b4266ee063383
Marat Nepomnyashy <maratbn@gmail.com>
2017-01-08 14:27:17 -0800

    Incremented the VERSION to '1.2.0-development_unreleased'.

--- webpack_in/entry.jsx
+++ webpack_in/entry.jsx
@@ -16,7 +16,7 @@
 
   Copyright (C) 2015-2017  Marat Nepomnyashy  http://maratbn.com  maratbn@gmail
 
-  Version:        1.1.0-development_unreleased
+  Version:        1.2.0-development_unreleased
 
   Module:         webpack_in/entry2.jsx
 
